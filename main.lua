7e50992131f0e47217c44d957afb5e08ae695c69
petergriffin16 <117768818+petergriffin16@users.noreply.github.com>
2024-05-30 13:04:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
-items = [17663889160,17663864200,17662292404,17662246635,17661962069]
+game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, 17663889160)
+--[[items = [17663889160,17663864200,17662292404,17662246635,17661962069]
 for index,itemid in pairs(items) do
   game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, itemid)
   sleep(8)
 end
+]]--
