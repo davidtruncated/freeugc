122bbd064bff0b4c7ea84fac16f9e5cd1827713e
david <bobsaggy82@gmail.com>
2024-11-04 12:42:58 -0500

    comment

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, 17663889160)
+itemid = 17663889160  -- change to whatever item you want to try and buy, works sometimes.
+game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, itemid)
 --[[items = [17663889160,17663864200,17662292404,17662246635,17661962069]
 for index,itemid in pairs(items) do
   game:GetService("MarketplaceService"):PromptPurchase(game.Players.LocalPlayer, itemid)
